414714dea43b7a0e182b24d9135017a3c3ef7dec
coder.flower <coder.flower@gmail.com>
2019-03-30 11:10:18 +0800

    添加MainViewController

---
+++ WorkFlow/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  WorkFlow
+//
+//  Created by 花菜 on 2019/3/30.
+//  Copyright © 2019 Coder.flower. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("test>>>>>>>>")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
